d0489df9f341e8a22a6352fc8b021d09b71f4670
Nishant-Soman-iitm <21f3002147@ds.study.iitm.ac.in>
2026-08-16 03:00:33 +0530

    x

--- js/main.js
+++ js/main.js
@@ -228,3 +228,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+// Initialize Vanta.NET in your initialization script
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff7f2a,
+    backgroundColor: 0x09090b,
+    points: 12.00,
+    maxDistance: 22.00,
+    spacing: 16.00
+  });
+});
